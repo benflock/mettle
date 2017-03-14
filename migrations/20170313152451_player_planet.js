47566cf82f851dad372ed8bbb3cf3255380c2f19
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-03-14 11:33:36 -0600

    added keys to joins

--- migrations/20170313152451_player_planet.js
+++ migrations/20170313152451_player_planet.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('player_planet', function(player_planet) {
     player_planet.integer('planet_id').references('planet.id');
     player_planet.integer('player_id').references('player.id');
+    player_planet.primary(['planet_id', 'player_id']);
   })
 };
 
